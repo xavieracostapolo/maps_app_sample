da9eb1b14ff96ce9d7c9f85320bf3b8f2f3b33de
Xavier Acosta Polo <xavieracosta@ingenieros.com>
2022-11-23 21:30:44 -0500

    Configurar GeoLocator y agregar metodos en el bloc

--- maps_app_sample/lib/blocs/gps/gps_bloc.dart
+++ maps_app_sample/lib/blocs/gps/gps_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -13,5 +14,29 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
           isGpsEnabled: event.isGpsEnabled,
           isGpsPermissionGranted: event.isGpsPermissionGranted));
     });
+
+    _init();
+  }
+
+  Future<void> _init() async {
+    final data = await _checkGpsStatus();
+    print('isEnabled $data');
+  }
+
+  Future<bool> _checkGpsStatus() async {
+    final isEnabled = await Geolocator.isLocationServiceEnabled();
+
+    Geolocator.getServiceStatusStream().listen((event) {
+      final isEnabled = (event.index == 1) ? true : false;
+      print('Service status $isEnabled');
+    });
+
+    return isEnabled;
+  }
+
+  @override
+  Future<void> close() {
+    // TODO: implement close
+    return super.close();
   }
 }
