b797b5d865843c7cf13384f7b908063686cca0c6
Xavier Acosta Polo <xavieracosta@ingenieros.com>
2022-11-23 21:04:43 -0500

    Agregar primer evento al bloc

--- maps_app_sample/lib/blocs/gps/gps_state.dart
+++ maps_app_sample/lib/blocs/gps/gps_state.dart
@@ -7,9 +7,17 @@ class GpsState extends Equatable {
   const GpsState(
       {required this.isGpsEnabled, required this.isGpsPermissionGranted});
 
+  GpsState copyWith(
+          {bool? isGpsEnabled, bool? isGpsPermissionGranted}) =>
+      GpsState(
+          isGpsEnabled: isGpsEnabled ?? this.isGpsEnabled,
+          isGpsPermissionGranted:
+              isGpsPermissionGranted ?? this.isGpsPermissionGranted);
+
   @override
   List<Object> get props => [isGpsEnabled, isGpsPermissionGranted];
 
   @override
-  String toString() => '{ isGpsEnabled : $isGpsEnabled, isGpsPermissionGranted : $isGpsPermissionGranted }';
+  String toString() =>
+      '{ isGpsEnabled : $isGpsEnabled, isGpsPermissionGranted : $isGpsPermissionGranted }';
 }
