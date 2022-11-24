b89c334418b7231bf32e149f07b0cb957e8d3be8
Xavier Acosta Polo <xavieracosta@ingenieros.com>
2022-11-23 20:25:10 -0500

    Agregar estructura de carpetas y pantalla de solicitar permisos

---
+++ maps_app_sample/lib/screens/gps_access_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class GpsAccessScreen extends StatelessWidget {
+  const GpsAccessScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: _AccessButton()),
+    );
+  }
+}
+
+class _AccessButton extends StatelessWidget {
+  const _AccessButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Es necesario el acceso al GPS'),
+        MaterialButton(
+            color: Colors.black,
+            shape: const StadiumBorder(),
+            elevation: 0,
+            splashColor: Colors.transparent,
+            onPressed: (() {
+              // TODO: Por hacer
+            }),
+            child: const Text(
+              'Solicitar Acceso',
+              style: TextStyle(color: Colors.white),
+            ))
+      ],
+    );
+  }
+}
+
+class _EnabledGpsMessage extends StatelessWidget {
+  const _EnabledGpsMessage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Debe habilitar el GPS',
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.w300),
+    );
+  }
+}
